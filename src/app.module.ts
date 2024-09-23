f7140376bd6701961c5e5913b44c5a06127212dc
VU AN KHANH <vuankhanh19071992@gmail.com>
2024-09-23 16:01:10 +0700

    refactor: Update default environment file path in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AlbumModule } from './module/album/album.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './module/auth/auth.module';
+import path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
-      envFilePath: '.env', //Phải định nghĩa mặc định ở đây vì khi chạy pm2 thì location (the project root directory) sẽ khác
+      envFilePath: path.join(__dirname, '.env'), //Phải định nghĩa mặc định ở đây vì khi chạy pm2 thì location (the project root directory) sẽ khác
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
