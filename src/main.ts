2cd2d41d9add4c46ab1cd82d1f097f02f66cd02e
vuankhanh <vuankhanh19071992@gmail.com>
2025-10-18 21:17:36 +0700

    run with https is optional

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 
+// import * as fs from 'fs';
+// import { join } from 'path';
 async function bootstrap() {
+  // const httpsOptions = {
+  //   key: fs.readFileSync(join(process.cwd(), 'server.key')),
+  //   cert: fs.readFileSync(join(process.cwd(), 'server.crt')),
+  // };
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  //   httpsOptions
+  // });
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const staticPath = configService.get('folder.album');
